fb325df20d077f8220522c7138d2885385c102ac
vitor-moymer <vitor@moymer.com>
2018-05-18 17:45:09 -0300

    Update http_gen_server.erl

--- src/http_gen_server.erl
+++ src/http_gen_server.erl
@@ -16,8 +16,8 @@
 -export([call/2, cast/2, make_call/3, make_cast/3]).
 %-export([ parallel_r/4,parallel_nr/3]).
 -define(SERVER, ?MODULE).
--define(CONNECTION_TIMEOUT, 1000).
--define(RECEIVE_TIMEOUT,5000).
+-define(CONNECTION_TIMEOUT, 5000).
+-define(RECEIVE_TIMEOUT,20000).
 -record(state, {}).
 
 start_link(Services) ->
