d91fcb391f336767a7606300b02bfa1d018f4426
vitor-moymer <vitor@moymer.com>
2018-11-10 09:15:37 -0200

    Update gen_server_connector.erl

--- src/gen_server_connector.erl
+++ src/gen_server_connector.erl
@@ -12,7 +12,7 @@ start_connector(Port, Connections) ->
                                             ]}
 				     ]),
     {ok, _} = cowboy:start_clear(http, [{port, Port}, {num_acceptors,Connections}], #{
-						      env => #{dispatch => Dispatch}, request_timeout => 120000, max_keepalive => 1000
+						      env => #{dispatch => Dispatch}, request_timeout => 300000, max_keepalive => 1000
 						     }).
 
 start_connector(Port) ->
